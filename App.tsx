da73789f8fe55354ab30ea926bd96126e8cb5a69
radwa radwan <radwaradwan142@gmail.com>
2024-05-29 14:55:11 +0300

    add splash screen as a screen

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import LoginScreen from './Components/Screens/LoginScreen';
 import SignUpScreen from './Components/Screens/SignUpScreen';
 import DrawerNavigator from './Components/Screens/DrawerNavigator';
+import SplashScreen from './Components/Screens/SplashScreen';
 // import BottomTabNavigator from './Components/Molecules/BottomTabNav';
 // import Input from './Components/Atoms/Input';
 // import AddBen from './Components/Organisms/AddBen';
@@ -27,6 +28,7 @@ function App() {
     <>
       <NavigationContainer>
         <Stack.Navigator>
+          <Stack.Screen name="splash" component={SplashScreen} options={{ headerShown: false }}/>
           <Stack.Screen name="login" component={LoginScreen} options={{ headerShown: false }}/>
           <Stack.Screen name="Drawer" component={DrawerNavigator} options={{ headerShown: false }} />
           <Stack.Screen name="signup" component={SignUpScreen} options={{ headerShown: false }}/>
---
+++ Components/Screens/SplashScreen.tsx
@@ -0,0 +1,56 @@
+import React, { useEffect } from 'react';
+import { View, Image, StyleSheet, StatusBar } from 'react-native';
+type Props = {
+    navigation:any,
+};
+function SplashScreen(props:Props) {
+    const {navigation} = props;
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            navigation.navigate('login'); // Replace 'Home' with your main screen
+        }, 2000);
+
+        return () => clearTimeout(timer);
+    }, [navigation]);
+
+    return (
+        <View style={styles.container}>
+            <StatusBar translucent backgroundColor="transparent"/>
+            <View style={styles.logo}>
+                <Image
+                    source={require('../../Assets/images/splashLogo.png')} // Replace with your logo path
+                />
+            </View>
+            <View style={styles.Textlogo}>
+                <Image
+                    source={require('../../Assets/images/splashTLogo.png')} // Replace with your logo path
+                />
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        // justifyContent: 'center',
+        // alignItems: 'center',
+        backgroundColor: '#F1F3FB',
+        // justifyContent: 'space-around',// Replace with your desired background color
+    },
+    logo: {
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+        // width: 150,
+        // height: 150, // Adjust these dimensions to fit your logo
+        // resizeMode: 'contain',
+    },
+    Textlogo:{
+        alignItems:'center',
+        justifyContent:'flex-end',
+        marginBottom:50,
+    },
+});
+
+export default SplashScreen;
